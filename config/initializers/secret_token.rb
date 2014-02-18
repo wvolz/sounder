b9c745fefe38fb89f4579f7c980d37817e09e365
Erin Brown <embrown@ufl.edu>
2014-02-17 19:39:04 -0500

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sounder::Application.config.secret_key_base = 'ba890004f8ab31d34d644af65126d87ff8d55b43b9d9203605adec0fbec20c3315f66a51ac9d36cb50cd8bb95a5bfe9957df042273910ef70dea4b425fecc7e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sounder::Application.config.session_store :cookie_store, key: '_sounder_session'
