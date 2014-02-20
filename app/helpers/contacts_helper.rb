d1f70fe05035def1da477d599f11770da8a5314a
Erin Brown <embrown@ufl.edu>
2014-02-19 20:20:37 -0500

    Added ARRL_SECTIONS.

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -1,4 +1,19 @@
 module ContactsHelper
+
+  ARRL_SECTIONS = [
+    "CT", "EMA", "ME", "NH", "RI", "VT", "WMA",                                  # 1
+    "ENY", "NLI", "NNJ", "NNY", "SNJ", "WNY",                                    # 2
+    "DE", "EPA", "MDC", "WPA",                                                   # 3
+    "AL", "GA", "KY", "NC", "NFL", "SC", "SFL", "WCF", "TN", "VA", "PR", "VI",   # 4
+    "AR", "LA", "MS", "NM", "NTX", "OK", "STX", "WTX",                           # 5
+    "EB", "LAX", "ORG", "SB", "SCV", "SDG", "SF", "SJV", "SV", "PAC",            # 6
+    "AZ", "EWA", "ID", "MT", "NV", "OR", "UT", "WWA", "WY", "AK",                # 7
+    "MI", "OH", "WV",                                                            # 8
+    "IL", "IN", "WI",                                                            # 9
+    "CO", "IA", "KS", "MN", "MO", "NE", "ND", "SD",                              # 0
+    "MAR", "NL", "QC", "ONN", "ONS", "ONE", "GTA", "MB", "SK", "AB", "BC", "NT"  # Canada
+  ]
+
   BAND = ["160m", "80m", "40m", "20m", "15m", "10m", "50m", "2m", "1.25m", "70cm", "33cm", "23cm"]
 
   MODE = ["CW", "Digital", "Phone"]
