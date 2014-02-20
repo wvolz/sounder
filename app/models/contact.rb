77048bb54cf55ecf83000cfb5afb129febae22ec
Erin Brown <embrown@ufl.edu>
2014-02-19 20:21:39 -0500

    Added validations for :band, :mode, :callsign, :classification, :section.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,18 @@
 class Contact < ActiveRecord::Base
+
+  # Check to make sure all fields are filled
+  validates :band, :mode, :callsign, :classification, :section, presence: true
+
+  # Is the band valid?
+  validates :band, inclusion: { in: ContactsHelper::BAND, message: "%{value} is not a valid band." }
+
+  # Is the mode valid?
+  validates :mode, inclusion: { in: ContactsHelper::MODE, message: "%{value} is not a valid mode." }
+
+  # Is the entry unique?
+  validates :callsign, uniqueness: { scope: :band, message: "%{value} is a duplicate entry." }
+
+  # Is the section valid?
+  validates :section, inclusion: { in: ContactsHelper::ARRL_SECTIONS, message: "%{value} is not a valid ARRL section." }
+
 end
