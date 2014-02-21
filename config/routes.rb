92ac40198670133080e8a4dc0f94d38cd9d73019
Erin Brown <embrown@ufl.edu>
2014-02-21 00:19:26 -0500

    Changed root index to contacts#new.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Sounder::Application.routes.draw do
   resources :contacts
 
-  root 'contacts#index'
+  root 'contacts#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
