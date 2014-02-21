da78ee456ef74cecc203976a48b2e49bbc226f9a
Erin Brown <embrown@ufl.edu>
2014-02-20 21:47:47 -0500

    Added DX to ARRL Sections.

--- app/helpers/contacts_helper.rb
+++ app/helpers/contacts_helper.rb
@@ -11,7 +11,8 @@ module ContactsHelper
     "MI", "OH", "WV",                                                            # 8
     "IL", "IN", "WI",                                                            # 9
     "CO", "IA", "KS", "MN", "MO", "NE", "ND", "SD",                              # 0
-    "MAR", "NL", "QC", "ONN", "ONS", "ONE", "GTA", "MB", "SK", "AB", "BC", "NT"  # Canada
+    "MAR", "NL", "QC", "ONN", "ONS", "ONE", "GTA", "MB", "SK", "AB", "BC", "NT", # Canada
+    "DX"                                                                         # International
   ]
 
   BAND = ["160m", "80m", "40m", "20m", "15m", "10m", "50m", "2m", "1.25m", "70cm", "33cm", "23cm"]
