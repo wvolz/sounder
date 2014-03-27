595ee7ec0ce843dc8895d60694dfbab9c5917194
Erin Brown <embrown@ufl.edu>
2014-03-27 15:01:56 -0400

    Added IP notice.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,5 @@
+require 'socket'
+
 module ApplicationHelper
 
   def format_time(datetime)
@@ -6,6 +8,11 @@ module ApplicationHelper
 
   def format_time_short(datetime)
     datetime.strftime("%H:%M")
+  end
+
+  def display_ip_address
+    ip_address = Socket.getaddrinfo(Socket.gethostname, 3000)
+    ip_address[0][2]
   end
 
 end
